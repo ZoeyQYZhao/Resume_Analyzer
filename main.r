0b75475b78cec57772487d684907e5a034757db3
ZoeyQYZhao <104409532+ZoeyQYZhao@users.noreply.github.com>
2022-04-26 04:30:39 -0400

    Add files via upload

---
+++ main.r
@@ -0,0 +1,24 @@
+
+
+# import init
+source("init.R")
+
+# import ui
+source("ui.R")
+
+# import server 
+source("server.R")
+
+# Score resume and occupation
+source("ScoreResume.R")
+
+# scrape Indeed
+source("ScrapeIndeed.R")
+
+# extract keywords
+source("ExtractKeywords.R")
+
+
+#shinyApp(ui = my_ui, server = shinyServer)
+
+
